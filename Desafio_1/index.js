a0621d1ff1de83c05a253ebcef5d2227de0c7779
Leandro Apablaza <lea.apagro@gmail.com>
2022-08-29 20:49:28 -0300

    Mejoras al desafio_1

--- Desafio_1/index.js
+++ Desafio_1/index.js
@@ -1,11 +1,13 @@
 let numero = parseInt(prompt("Ingrese numero a evaluar o 0 para salir:: "));
 
-while (numero != 0) {
+while (numero !== 0) {
     
-    if (numero % 2 == 0) {
+    if (numero % 2 === 0) {
         alert("Su numero es par");
     } else {
         alert("Su numero es impar");
     }
     numero = parseInt(prompt("Ingrese numero a evaluar o 0 para salir: "));
 }
+
+alert("Gracias por usar el programa");
